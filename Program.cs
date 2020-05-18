3b65e2167cf2d30fd6f403126adc408af1cd7032
ramtinbozorgi@web.de <ramtinbozorgi@web.de>
2020-05-19 01:00:33 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            int s = 0;
+            string n = Console.ReadLine();
+            int x = n.Length;
+            if (x > 0)
+            {
+                for (int i = 0; i < x; i++)
+                {
+
+                    s += int.Parse(n[i].ToString());
+                }
+                Console.WriteLine("The sum of {0} digit of number {1} is: {2}.", x, n, s);
+
+            }
+            else
+            {
+                Console.WriteLine("Atleast 1 number is essential.");
+            }
+        }
+    }
+}
